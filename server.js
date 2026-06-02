0fb760922b68ff0f1db032859800cb64c3bcd42c
helloprincesingh <princesingh23032006@gmail.com>
2026-06-03 04:30:23 +0530

    Final Vercel routing fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let currentPort = BASE_PORT;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('./'));
+app.use(express.static(__dirname));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
